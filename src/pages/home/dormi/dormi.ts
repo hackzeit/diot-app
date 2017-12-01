05a77269a1f4e66bb70a20d3d7021d81eff8201a
Frank Moreno <frankmoreno1993@gmail.com>
2017-11-30 19:01:53 -0500

    starting dormitorio page

---
+++ src/pages/home/dormi/dormi.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-dormi',
+  templateUrl: 'dormi.html'
+})
+export class DormiPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
