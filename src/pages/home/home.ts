ed23a86caa5b26b2a5bd465a561e9827eb7523b9
Frank Moreno <frankmoreno1993@gmail.com>
2017-11-30 19:02:15 -0500

    home: a little modification

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { SalaPage } from './sala/sala';
+import { DormiPage } from './dormi/dormi';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  places = [
+    {
+      "room":'Sala',
+      "page":SalaPage
+    },{
+      "room":'Dormitorio',
+      "page":DormiPage
+    },
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToPage(OtherPage) {
+    this.navCtrl.push(OtherPage);
   }
 
 }
